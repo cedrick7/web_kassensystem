b41e3c08925ed561794cab05fa15a3c5977bbacf
Cedrick CF <ce.candia@icloud.com>
2020-06-14 13:40:21 +0200

    php

---
+++ kasse.php
@@ -0,0 +1,34 @@
+<?php
+// Catch Button Attribute
+$button = $_POST[".product"];
+
+//SQL DATABASE Setup
+$servername = "https://hosting.iem.thm.de/phpmyadmin/";
+$username = "jfhs38";
+$password = "5Zcxh4eRqyRhwPaG";
+$dbname = "jfhs38_1";
+$endprice = 0;
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//SQL Statements
+$sql = "Select Preis from Produkte where name is '$button' ";
+$endprice += $conn->query($sql);
+$sql = "Select * from Produkte where name is '$button' ";
+$product = $conn->query($sql);
+?>
+
+<script>
+    // Javascript variable übertragen
+    var product = <?php echo json_encode($product); ?>;
+</script>
+
+<script>
+    // Javascript variable übertragen
+    var endprice = <?php echo json_encode($endprice); ?>;
+</script>
