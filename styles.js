299f1fdeee9e1930f3421e7b049bf31e2f28a4d5
Cedrick CF <ce.candia@icloud.com>
2020-06-16 18:07:43 +0200

    x

--- styles.js
+++ styles.js
@@ -64,3 +64,23 @@ function overrideResult(price) {
   // gesamtpreis anpassen
   document.getElementById("price_result").innerHTML = Math.round((price_result + price) * 100) / 100;
 }
+
+
+
+
+// calculator
+const screen = document.getElementById('screen');
+
+function clear_screen() {
+  screen.value = " ";
+}
+
+function display_on_screen(x) {
+  screen.value += x;
+}
+
+function calculate_the_result() {
+  let val = screen.value;
+  val = eval(val);
+  screen.value = val;
+}
