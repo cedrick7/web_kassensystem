8119bd60ca1a49e3643c69667200c3708673787e
Cedrick CF <ce.candia@icloud.com>
2020-07-01 01:13:02 +0200

    javascript works! - add items to cart - delete items from card by id - create bill - print bill - use calculator - enter and exit fullscreen mode
    + added contact.html

--- index.php
+++ index.php
@@ -41,58 +41,63 @@ if(isset($_POST['Productbutton'])){
             <input id="load_data" type="submit" name="load_data" value="load_data" onclick="generatebuttons()" />
         </form>
 
+        <div class="product-box" attribute="">
+          <a href="javascript:createCart();" onclick="" id="" class="product">
+            INIT
+          </a>
+        </div>
 
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-1" class="product">
+         <a href="#" onclick="addToCart(1, 'produkt 1', 9.99)" id="product-1" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Laugenbretzel">
            </form>
          </a>
        </div>
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-2" class="product">
+         <a href="#" onclick="addToCart(2, 'produkt 2', 9.99)" id="product-2" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Weizen-Mischbrot 250g">
            </form>
          </a>
        </div>
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-3" class="product">
+         <a href="#" onclick="addToCart(3, 'produkt 3', 9.99)" id="product-3" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Roggen-Mischbrot 250g">
            </form>
          </a>
        </div>
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-4" class="product">
+         <a href="#" onclick="addToCart(1, 'produkt 1', 9.99)" id="product-4" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Gutsherren-Brot 500g">
            </form>
          </a>
        </div>
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-5" class="product">
+         <a href="#" onclick="addToCart(1, 'produkt 1', 9.99)" id="product-5" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Kosakenbrot 750 gg">
            </form>
          </a>
        </div>
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-6" class="product">
+         <a href="#" onclick="addToCart(1, 'produkt 1', 9.99)" id="product-6" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Laugenbretzel">
            </form>
          </a>
        </div>
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-7" class="product">
+         <a href="#" onclick="addToCart(1, 'produkt 1', 9.99)" id="product-7" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Fit-Berrybrot 500g">
            </form>
          </a>
        </div>
        <div class="product-box" attribute="">
-         <a href="#" onclick="addToCart('produkt 1', 9.99)" id="product-8" class="product">
+         <a href="#" onclick="addToCart(1, 'produkt 1', 9.99)" id="product-8" class="product">
            <form method="post">
              <input type="submit" name="Productbutton" value="Milchbrötchen">
            </form>
@@ -220,35 +225,41 @@ if(isset($_POST['Productbutton'])){
        <div class="sum-list">
          <p>Preis Gesamt = <span id="price_result">0.00</span>€</p>
        </div>
+       <div class="remove-listItem">
+         <form>
+           <input type="text" id="remove-this remove-input" placeholder="ID des zu entfernenden Produktes..."/>
+           <input type="button" id="remove-button" onclick="deleteCardItem()" value="Entfernen"/>
+         </form>
+       </div>
        <div class="checkout-box">
-         <a href="javascript:generateBill()" id="saveAsPDF">Rechnung erstellen</a>
+         <a href="javascript:generateBill()" id="saveBill">Rechnung erstellen</a>
          <a href="" id="sumbit">zur Kasse gehen</a>
        </div>
        <div class="calculator">
          <form>
-           <input type="text" id="calc-display">
-           <br>
-           <input type="button" class="keys" value="7" onclick="addToScreen('7')">
-           <input type="button" class="keys" value="8" onclick="addToScreen('8')">
-           <input type="button" class="keys" value="9" onclick="addToScreen('9')">
-           <input type="button" class="keys" value="C" onclick="addToScreen('c')">
-           <input type="button" class="keys" value="CE" onclick="backspace()">
-           <br>
-           <input type="button" class="keys" value="4" onclick="addToScreen('4')">
-           <input type="button" class="keys" value="5" onclick="addToScreen('5')">
-           <input type="button" class="keys" value="6" onclick="addToScreen('6')">
-           <input type="button" class="keys" value="*" onclick="addToScreen('*')">
-           <input type="button" class="keys" value="/" onclick="addToScreen('/')">
-           <br>
-           <input type="button" class="keys" value="1" onclick="addToScreen('1')">
-           <input type="button" class="keys" value="2" onclick="addToScreen('2')">
-           <input type="button" class="keys" value="3" onclick="addToScreen('3')">
-           <input type="button" class="keys" value="+" onclick="addToScreen('+')">
-           <input type="button" class="keys" value="-" onclick="addToScreen('-')">
-           <br>
-           <input type="button" class="keys" id="key-zero" value="0" onclick="addToScreen('0')">
-           <input type="button" class="keys" value="." onclick="addToScreen('.')">
-           <input type="button" class="equal" id="key-result" value="=" onclick="result()">
+           <input type="text" id="calc-display" />
+           <br></br>
+           <input type="button" class="keys" value="7" onclick="addToScreen('7')" />
+           <input type="button" class="keys" value="8" onclick="addToScreen('8')" />
+           <input type="button" class="keys" value="9" onclick="addToScreen('9')" />
+           <input type="button" class="keys" value="C" onclick="addToScreen('c')" />
+           <input type="button" class="keys" value="CE" onclick="backspace()" />
+           <br></br>
+           <input type="button" class="keys" value="4" onclick="addToScreen('4')" />
+           <input type="button" class="keys" value="5" onclick="addToScreen('5')" />
+           <input type="button" class="keys" value="6" onclick="addToScreen('6')" />
+           <input type="button" class="keys" value="*" onclick="addToScreen('*')" />
+           <input type="button" class="keys" value="/" onclick="addToScreen('/')" />
+           <br></br>
+           <input type="button" class="keys" value="1" onclick="addToScreen('1')" />
+           <input type="button" class="keys" value="2" onclick="addToScreen('2')" />
+           <input type="button" class="keys" value="3" onclick="addToScreen('3')" />
+           <input type="button" class="keys" value="+" onclick="addToScreen('+')" />
+           <input type="button" class="keys" value="-" onclick="addToScreen('-')" />
+           <br></br>
+           <input type="button" class="keys" id="key-zero" value="0" onclick="addToScreen('0')" />
+           <input type="button" class="keys" value="." onclick="addToScreen('.')" />
+           <input type="button" class="equal" id="key-result" value="=" onclick="result()" />
          </form>
        </div>
      </section>
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* Vollbildmodus -------------------------------------------------------------*/
 // In den Vollbildmodus wechseln
 function enterFullscreen(element) {
   if (element.requestFullscreen) {
@@ -28,9 +29,7 @@ function exitFullscreen() {
   document.getElementById("fullscreen_on").style.display = "block";
 }
 
-
-// Funktionen des Taschenrechners
-
+/* Taschenrechner ------------------------------------------------------------*/
 // angeklickte Werte zum TR-Display hinzufügen
 function addToScreen(x) {
   var box = document.getElementById("calc-display");
@@ -62,35 +61,113 @@ function backspace() {
 }
 
 
-// Produkte zum Warenkorb hinzufügen
-
-function addToCart(product_name, product_price) {
-  // finde alle wichtigen Elemente
-  var list = document.getElementById("product-list");
-
-  // werte der produkte herausfinden und in variablen speichern
-  // var product_item = document.getElementById("product-01");
-  // var product_name = (document.getElementById("product-01_name").innerHTML).toString();
-  // var product_price_str = (document.getElementById("product-01_price").innerHTML).toString();
-  // var product_price = Number(product_price_str.replace(/[^0-9.-]+/g, ""));
+/* Warenkorb, Rechnung -------------------------------------------------------*/
+// Warenkorb initialisieren
+function createCart() {
+  // produkt aus warenkorb entfernen button unanklickbar machen
+  document.getElementById("remove-button").disabled = true;
+  document.getElementById("remove-input").disabled = true;
 
   // array für ausgewählte produkte erstellen
   var cart = [
-    [1, "Keine Produkte ausgewählt!", 0.00]
+    [0, 0, "Keine Produkte ausgewählt!", 0.00]
   ]
 
+  // warenkorb als localStorage speichern
+  localStorage["cart"] = JSON.stringify(cart);
+  // // oder mit:
+  // var cart_serialized = JSON.strinigy(cart);
+  // localStorage.setItem("cart", cart_serialized);
+  // // und andersrum:
+  // let cart_deserialized = JSON.parse(localStorage.getItem("cart"));
+  // console.log(cart_deserialized);
+}
+
+// Produkte zum Warenkorb hinzufügen
+function addToCart(product_id, product_name, product_price) {
+  // warenkorb aus localStorage laden
+  var cart = JSON.parse(localStorage["cart"]);
+
   // ausgewählte produkte zum array hinzufügen
-  cart.push([1, product_name, product_price]);
+  cart.push([1, product_id, product_name, product_price]);
+
+  // warenkorb als localStorage speichern
+  localStorage["cart"] = JSON.stringify(cart);
+
+  getCart(); // warenkorb aktualisieren
+  overrideResult(product_price, "+"); // gesamtpreis aktualisieren
+}
+
+// produkt aus warenkorb löschen
+function deleteCardItem() {
+  // produkt-id filtern
+  var product_id_str = document.getElementById("remove-this").value;
+  var product_id = Number(product_id_str.replace(/[^0-9.-]+/g, ""));
+
+  // warenkorb aus localStorage laden
+  var cart = JSON.parse(localStorage["cart"]);
+
+  // im warenkorb nach produkt-id suchen und
+  // warenkorb-position und preis aus zu löschendem produkt filtern
+  var product_id_found = false;
+  for (var i = 0; i <= cart.length - 1; i++) {
+    if (cart[i][1] == product_id) {
+      var product_position = cart[i][0];
+      var product_price = cart[i][3];
+      product_id_found = true;
+
+      // überprüfen ob preis gefiltert wird
+      if (product_price == null) {
+        console.error("no price found! " + product_price);
+      }
+
+      // produkt aus warenkorb entfernen
+      for (var i = 0; i <= cart.length - 1; i++) {
+        if (cart[i][1] == product_id) {
+          cart.splice(i, 1);
+          break; // produkt gefunden, also suche abbrechen
+        }
+      }
+
+      // warenkorb als localStorage speichern
+      localStorage["cart"] = JSON.stringify(cart);
+
+      getCart(); // warenkorb aktualisieren
+      overrideResult(product_price, "-"); // gesamtpreis aktualisieren
+      break; // suchen stoppen, weil schon gefunden
+    }
+  }
+  if (product_id_found == false) {
+    console.error("product-id " + product_id + " not in card!");
+    overrideResult(0.00, "x"); // gesamtpreis aktualisieren
+  }
+
+  // produkt-id-eingabe aus dem input-feld löschen
+  document.getElementById("remove-this").value = "";
+}
+
+// warenkorb anzeigen
+function getCart() {
+  // finde alle wichtigen Elemente
+  var list = document.getElementById("product-list");
+
+  // bisherigen warenkorb-inhalt löschen
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+
+  // warenkorb aus localStorage laden
+  var cart = JSON.parse(localStorage["cart"]);
 
   // ausgewählte produkte zum warenkorb hinzufügen und gesamtpreis anpassen
   for (var i = 1; i <= cart.length - 1; i++) {
     var productCount = (cart[i][0]).toString();
-    var productName = cart[i][1];
-    var productPrice = (cart[i][2]).toString();
-    var product = productName + "  –  " + productPrice + "€";
-    //var product = productCount + "x " + productName + ", " + productPrice + "€";
+    var productId = cart[i][1];
+    var productName = cart[i][2];
+    var productPrice = (cart[i][3]).toString();
+    var product = productName + "  –  " + productPrice + "€       (id:" + productId + ")";
     console.log(product);
-    price_result += cart[i][2];
+    var price_result = cart[i][3];
 
     // erstelle ein neues Element li, das später in unsere Liste aufgenommen wird
     var item = document.createElement("li");
@@ -101,45 +178,53 @@ function addToCart(product_name, product_price) {
     // Element zu der Liste hinzufügen
     list.appendChild(item);
   }
-
-  overrideResult(product_price);
 }
 
 // gesamtpreis anpassen
-function overrideResult(price) {
+function overrideResult(price, operator) {
   // finde alle wichtigen Elemente
-  var result = document.getElementById("price_result");
-
+  //var result = document.getElementById("price_result");
 
   // werte der produkte herausfinden und in variablen speichern
   var price_result_str = (document.getElementById("price_result").innerHTML).toString();
   var price_result = Number(price_result_str.replace(/[^0-9.-]+/g, ""));
 
-  // Platzhalter
-  function countPlaceholder() {
-    // finde alle wichtigen Elemente
-    var list = document.getElementById("product-list");
-
-    var placeholder_check = false;
-    if (placeholder_check) {
-      list.removeChild(list.childNodes[0]);
-      placeholder_check = false;
+  // gesamtpreis anpassen
+  if (operator == "+") {
+    document.getElementById("price_result").innerHTML = Math.round((price_result + price) * 100) / 100;
+    document.getElementById("remove-button").disabled = false;
+    document.getElementById("remove-input").disabled = false;
+  } else if (operator == "-") {
+    document.getElementById("price_result").innerHTML = Math.round((price_result - price) * 100) / 100;
+    var price_result_str = (document.getElementById("price_result").innerHTML).toString();
+    var price_result = Number(price_result_str.replace(/[^0-9.-]+/g, ""));
+    if (price_result == 0 | price_result == null) {
+      document.getElementById("remove-button").disabled = true;
+      document.getElementById("remove-input").disabled = true;
+    } else {
+      document.getElementById("remove-button").disabled = false;
+      document.getElementById("remove-input").disabled = false;
+    }
+  } else {
+    var price_result_str = (document.getElementById("price_result").innerHTML).toString();
+    var price_result = Number(price_result_str.replace(/[^0-9.-]+/g, ""));
+    if (price_result == 0 | price_result == null) {
+      document.getElementById("remove-button").disabled = true;
+      document.getElementById("remove-input").disabled = true;
+    } else {
+      document.getElementById("remove-button").disabled = false;
+      document.getElementById("remove-input").disabled = false;
     }
   }
-
-  // gesamtpreis anpassen
-  document.getElementById("price_result").innerHTML = Math.round((price_result + price) * 100) / 100;
 }
 
-
-// Rechnung als PDF speichern
-
+// Rechnung erstellen (+ drucken)
 function generateBill() {
+  // warenkorb in variable speichern
+  var cartContent = document.getElementById("product-list").innerHTML;
+  sessionStorage.setItem("copyCartContent", cartContent);
 
-  var pageContent = document.getElementById("product-list").innerHTML;
-  sessionStorage.setItem("page1content", pageContent);
-
+  // warenkorb in neuem fenster öffnen (+ zeit und option "drucken" hinzufügen)
   var bill = window.open("");
-  bill.document.write("<!DOCTYPE html><html lang='de' dir='ltr'><head><meta charset='utf-8'/><title>bill</title></head><body style='text-align:center;'><p>Rechnung vom <span id='date'></span><script>var today = new Date(); document.getElementById('date').innerHTML = today;</script><ul id='createdBill' style='list-style-type:none;'>" + sessionStorage.getItem('page1content') + "</ul><br></br><br></br><button onclick='window.print()' style='background-color:#dddada;color: #000;text-align: center;text-decoration: none;padding: 10px;'>Rechnung drucken</button></body></html>");
-
+  bill.document.write("<!DOCTYPE html><html lang='de' dir='ltr'><head><meta charset='utf-8'/><title>bill</title></head><body style='text-align:center;'><p>Rechnung vom <span id='date'></span><script>var today = new Date(); document.getElementById('date').innerHTML = today;</script><ul id='createdBill' style='list-style-type:none;'>" + sessionStorage.getItem('copyCartContent') + "</ul><br></br><br></br><button onclick='window.print()' style='background-color:#dddada;color: #000;text-align: center;text-decoration: none;padding: 10px;'>Rechnung drucken</button></body></html>");
 }
